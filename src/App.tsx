79c6f05a60ba8df3b43eabf4934dff22992a42a2
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-01-25 13:29:26 -0300

    test commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,11 @@ import copyIcon from "./assets/copy-icon.svg";
 // import { Habit } from "./components/Habit";
 
 import "./styles/global.css";
-import { AlertComponent } from "./components/Alert";
 
 export function App() {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
-        <AlertComponent
-          alertType="INFORMATION"
-          title="Alerta"
-          content="Isso é um alerta"
-        />
         <Header />
         <SummaryTable />
       </div>
