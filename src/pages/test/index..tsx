455e91901874e4df67e51be0598f5111a2126b7c
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-03-07 11:48:46 -0300

    feat: add initial layout

---
+++ src/pages/test/index..tsx
@@ -0,0 +1,15 @@
+export const PageTest = () => {
+  return (
+    <div className="flex w-max">
+      <div className="flex w-[28.25rem] flex-row items-start">
+        <div className="flex w-[21.75rem] items-center">Avatar</div>
+
+        <div className="max-w-[1rem] flex-grow order-1 ">
+          <div className="w-[0.125rem] h-[0.125rem] bg-[#B2B2B2] rounded-full" />
+          <div className="w-[0.125rem] h-[0.125rem] bg-[#B2B2B2] rounded-full" />
+          <div className="w-[0.125rem] h-[0.125rem] bg-[#B2B2B2] rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
+};
