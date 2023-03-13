802ef7d9b91f7d94c0c693d864b42952af4c8413
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2023-03-13 01:18:40 -0300

    feat: add completed percentage props

--- src/components/HabitDay.tsx
+++ src/components/HabitDay.tsx
@@ -1,11 +1,14 @@
 import * as Popover from "@radix-ui/react-popover";
 import { ProgressBar } from "./ProgressBar";
 
-// interface HabitDayProps {
-//   completed: number;
-// }
+interface HabitDayProps {
+  completed: number;
+  amount: number;
+}
 
-export function HabitDay() {
+export function HabitDay({ completed, amount }: HabitDayProps) {
+  const completedPercentage = Math.round((completed / amount) * 100);
+
   return (
     <Popover.Root>
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
@@ -15,7 +18,7 @@ export function HabitDay() {
           <span className="font-semibold text-zinc-400">terça-feira</span>
           <span className="mt-1 font-extrabold leading-tight text-3xl">17/01</span>
 
-          <ProgressBar progress={85} />
+          <ProgressBar progress={completedPercentage} />
 
           <Popover.Arrow className="fill-zinc-900" height={8} width={16} />
         </Popover.Content>
